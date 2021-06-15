b41f8607e32083cebdf5c73761ed3d14f34150eb
Barbara <barbara.estevez@gmail.com>
2021-06-16 00:13:17 +0200

    get data from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import '../stylesheets/index.scss';
-import '../stylesheets/App.scss';
+import React, { useState, useEffect } from "react";
+import "../stylesheets/index.scss";
+import "../stylesheets/App.scss";
+import getDataFromApi from "../services/API";
 
-class App extends React.Component {
-  render() {
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(
+    () => {
+      // pedir datos de personajes
+      getDataFromApi().then(charactersData => {
+        setCharacters(charactersData);
+      });
+    },
+    [] /*parámetro que nos indica cuándo se debe ejecutar el useEffect*/
+  );
   return (
-    <div className="main__container" >
-      
-       <main>
-        
+    <div className="main__container">
+      <main>
+        <h1>Ricky and Morty</h1>
       </main>
     </div>
   );
-}
-}
+};
 
 export default App;
---
+++ src/services/API.js
@@ -0,0 +1,22 @@
+const getDataFromApi = () => {
+    // obtengo los datos del api
+    // in case we've been banned use this json --> https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json
+    return fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => {
+      // recorro el array, cojo los datos que me interesan y lo guardo en otro arrya (cleanData)
+        const cleanData = data.results.map((character) => {
+            return {
+              image: character.image,
+              name: character.name,
+              species: character.species,
+          //  id: character.id,
+          //  gender: character.gender,
+          //  status: character.status,
+            };
+          });
+          return cleanData;
+      });
+  };
+  
+  export default getDataFromApi;
